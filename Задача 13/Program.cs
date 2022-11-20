fb26b300bfece61410a9cc0412a8c2c89d90e5b9
Atlant <cuz.den30@gmail.com>
2022-11-21 01:43:22 +0300

    задача 13.01

--- Задача 13/Program.cs
+++ Задача 13/Program.cs
@@ -1,16 +1,21 @@
 ﻿Console.Write("Введите число:");
 int numb = Convert.ToInt32(Console.ReadLine());
- if(numb<100)
+ if(numb<100 && numb>-100)
 {
     Console.WriteLine("Не существует 3-й цифры");
+    
 }
 else 
 {
-    while(numb > 999)
+    while(numb > 999 || numb<-999)
     {
         numb = numb /10;
     }
   
     int numb2 =numb%100%10 ; 
+    if(numb2<0)
+    {
+        numb2=numb2*-1;
+    }
     Console.WriteLine("3-м числом является" + " " + numb2);  
 } 
