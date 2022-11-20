e53b18050683bc0def7fcfe17ff4fd558d926057
Atlant <cuz.den30@gmail.com>
2022-11-21 00:12:59 +0300

    задача 10

---
+++ Задача10/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите 3-х значное число: ");
+int numb = Convert.ToInt32(Console.ReadLine());
+if (numb > 100 && numb < 999 )
+{
+    int numb2 = numb/10 - numb/100*10;
+    Console.WriteLine("Вторая цифра числа" + " " + numb2 );
+}
+else
+{
+    Console.WriteLine("Неверное число. Попробуйте снова");
+    
+}
