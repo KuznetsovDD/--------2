9b698ad3c9f4b5ded460d7f01a194c83f7abe815
Atlant <cuz.den30@gmail.com>
2022-11-21 03:55:13 +0300

    задача 15

---
+++ Задача 15/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.Write("Введите число обозначающее день недели:");
+int numb = Convert.ToInt32(Console.ReadLine());
+
+if(numb<5&&numb>0)
+{
+    
+    Console.WriteLine("Выходной");
+}
+else
+{
+    Console.WriteLine("Не выходной");
+} 
+
+
+
+
+/*  
+if(numb == 1 || numb == 2 || numb == 3 || numb == 4 || numb == 5 )
+{
+    Console.WriteLine("Выходной");
+}
+else
+{
+    Console.WriteLine("Не выходной");
+}  */ 
+
+ 
+
+/* int [] arry = { 1, 2, 3, 4, 5 };
+int index =0 ;
+int long1 = arry.Length; 
+
+if(numb>5)
+{
+   Console.WriteLine("Не выходной"); 
+}
+while(index<long1)
+{
+    if(arry[index]==numb)
+    {
+        Console.WriteLine("Выходной");
+        break; 
+                
+    }
+    index++; 
+}
+*/
